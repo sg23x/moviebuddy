8bf3ec928c7f6495e10e7ad876dade6f598acbc8
sg23x <soumyagupta2307@gmail.com>
2021-08-16 13:52:39 +0530

    added: movie list item widget

---
+++ lib/widgets/movieListItem.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class MovieListItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      margin: EdgeInsets.only(
+        left: 5,
+        right: 5,
+        top: 5,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.pink,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(width: 2, color: Colors.black),
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 40,
+          ),
+          Column(
+            children: [
+              Text('Shershaah'),
+              Text('by Kiara'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
